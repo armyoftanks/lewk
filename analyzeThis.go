d53326a004e83ef459bb185c97b7f3f491299451
Armyoftanks <romina.skayanyc@gmail.coms>
2018-09-05 19:18:00 -0400

    new program image analyzer

---
+++ analyzeThis.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "github.com/gorilla/websocket"
+)
+
+// create twilio 2 way messenger
+// create a websocket
+// implement microsoft vision api
+// feed image data back into twilio through websocket
+
+func webSock() {
+
+  //attempting to open a websocket connection
+  origin := "http://localhost/"
+  url := "ws://localhost:12345/ws"
+  ws, err := websocket.Dial(url, "", origin)
+  if err != nil {
+      log.Fatal(err)
+  }
+  if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
+      log.Fatal(err)
+  }
+  var msg = make([]byte, 512)
+  var n int
+  if n, err = ws.Read(msg); err != nil {
+      log.Fatal(err)
+  }
+  fmt.Printf("Received: %s.\n", msg[:n])
+  
+}
+
+func chat() {
+
+}
+
+func analyzer() {
+
+}
+
+func main() {
+
+
+
+}
