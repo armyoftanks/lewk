45c4c0d3cf00dca961cfae462258a8fdc79c9115
Romina Simanovskaya <RSimanovskaya@ipsoft.com>
2018-09-06 06:38:04 -0400

    syncing stuff

--- analyzeThis.go
+++ analyzeThis.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "golang.org/x/net/websocket"
+	"fmt"
+	"log"
+
+	"golang.org/x/net/websocket"
 )
 
 // create twilio 2 way messenger
@@ -12,28 +13,30 @@ import (
 // feed image data back into twilio through websocket
 
 func Dial() {
-  // do i need this?
+	// do i need this?
 }
 
+type data struct {
+}
 
 func webSock() {
 
-  //attempting to open a websocket connection
-  origin := "http://localhost/"
-  url := "ws://localhost:12345/ws"
-  ws, err := websocket.Dial(url, "", origin)
-  if err != nil {
-      log.Fatal(err)
-  }
-  if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
-      log.Fatal(err)
-  }
-  var msg = make([]byte, 512)
-  var n int
-  if n, err = ws.Read(msg); err != nil {
-      log.Fatal(err)
-  }
-  fmt.Printf("Received: %s.\n", msg[:n])
+	//attempting to open a websocket connection
+	origin := "http://localhost/"
+	url := "ws://localhost:12345/ws"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
+		log.Fatal(err)
+	}
+	var msg = make([]byte, 512)
+	var n int
+	if n, err = ws.Read(msg); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Received: %s.\n", msg[:n])
 
 }
 
@@ -46,7 +49,5 @@ func analyzer() {
 }
 
 func main() {
-
-
 
 }
