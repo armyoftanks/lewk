0a4d1f7594c20189043756aa44a1ba6212cfd822
Romina Simanovskaya <RSimanovskaya@ipsoft.com>
2018-09-12 05:08:36 -0400

    added ngrok and webhook

--- analyzeThis.go
+++ analyzeThis.go
@@ -5,27 +5,40 @@ package main
 // implement microsoft vision api
 // feed image data back into twilio through websocket
 
-func Dial() {
-	// do i need this?
-}
+import (
+	"fmt"
 
-type data struct {
-}
+	"net/http"
 
-func webHook() {
+	"gopkg.in/go-playground/webhooks.v5/github"
+)
 
-	//attempting to open a websocket connection
-
-}
-
-func chat() {
-
-}
-
-func analyzer() {
-
-}
+const (
+	path = "/webhooks"
+)
 
 func main() {
+	hook, _ := github.New(github.Options.Secret("MyGitHubSuperSecretSecrect...?"))
 
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// ok event wasn;t one of the ones asked to be parsed
+			}
+		}
+		switch payload.(type) {
+
+		case github.ReleasePayload:
+			release := payload.(github.ReleasePayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", release)
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", pullRequest)
+		}
+	})
+	http.ListenAndServe(":3000", nil)
 }
