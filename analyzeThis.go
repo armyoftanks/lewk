f0f6397dadf85cd2ad6261aa4beab6c7b19c3d5f
Romina Simanovskaya <RSimanovskaya@ipsoft.com>
2018-09-12 05:24:51 -0400

    added vision api code

--- analyzeThis.go
+++ analyzeThis.go
@@ -6,7 +6,11 @@ package main
 // feed image data back into twilio through websocket
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
 
 	"net/http"
 
@@ -16,6 +20,64 @@ import (
 const (
 	path = "/webhooks"
 )
+
+func twilChat() {
+
+}
+
+func analyzeImage(imageUrl string) {
+	// For example, subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+	const subscriptionKey = "xxx"
+
+	// You must use the same location in your REST call as you used to get your
+	// subscription keys. For example, if you got your subscription keys from
+	// westus, replace "westcentralus" in the URL below with "westus".
+	const uriBase = "https://westus.api.cognitive.microsoft.com/vision/v2.0/analyze"
+
+	const params = "?visualFeatures=Description&details=Landmarks&language=en"
+	const uri = uriBase + params
+	imageUrlEnc := "{\"url\":\"" + imageUrl + "\"}"
+
+	reader := strings.NewReader(imageUrlEnc)
+
+	// Create the Http client
+	client := &http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	// Create the Post request, passing the image URL in the request body
+	req, err := http.NewRequest("POST", uri, reader)
+	if err != nil {
+		panic(err)
+	}
+
+	// Add headers
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Ocp-Apim-Subscription-Key", subscriptionKey)
+
+	// Send the request and retrieve the response
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// Read the response body.
+	// Note, data is a byte array
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse the Json data
+	var f interface{}
+	json.Unmarshal(data, &f)
+
+	// Format and display the Json result
+	jsonFormatted, _ := json.MarshalIndent(f, "", "  ")
+	fmt.Println(string(jsonFormatted))
+}
 
 func main() {
 	hook, _ := github.New(github.Options.Secret("MyGitHubSuperSecretSecrect...?"))
